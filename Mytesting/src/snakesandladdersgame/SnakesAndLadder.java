01373551795cb0ee8ae75167f03efcff472c6732
Chintan Killedar <chintan.killedar@siemens.com>
2019-09-19 16:59:58 +0530

    TurnsReverse

--- Mytesting/src/snakesandladdersgame/SnakesAndLadder.java
+++ Mytesting/src/snakesandladdersgame/SnakesAndLadder.java
@@ -4,6 +4,8 @@
  */
 package snakesandladdersgame;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.stream.Collectors;
 
 // there is only one snake
@@ -25,17 +27,29 @@ public class SnakesAndLadder {
     Board board = boardBuilder.addSnake(snake1).addSnake(greenSnake)
         .addLadder(ladder).addPlayer(komal).addPlayer(chintan).build();
     int position = 0;
+    int countOfTurns = 1;
+
 
     while (position < board.getSize()) {
-      for (Player player : board.getPlayers()) {
-      int numberDrawn = player.rollDice(dice);
-      System.out.println("Number drawn: " + numberDrawn);
+      board.getPlayers().forEach(s -> System.out.println(s.getName()));
+      if (countOfTurns % 5 == 0) {
+        Collections.reverse(board.getPlayers());
+        board.getPlayers().forEach(s -> System.out.println("REverse " + s.getName()));
+      }
+      Iterator<Player> playerIterator = board.getPlayers().listIterator();
+      while (playerIterator.hasNext()) {
+        Player player = playerIterator.next();
+        int numberDrawn = player.rollDice(dice);
+        System.out.println("Number drawn: " + numberDrawn);
         position = board.movePlayer(numberDrawn, player);
-      System.out.println("player name " + player.getName() + "new position :" + position);
+        System.out.println("player name " + player.getName() + "new position :" + position);
         if (position == 100) {
           break;
         }
       }
+      countOfTurns++;
+      System.out.println("The number of Turns:-" + countOfTurns);
+
     }
 
     System.out.println("THE WINNER :- " + board.getPlayers().stream().filter(s -> s.getPosition() == 100).collect(Collectors.toList()).get(0).getName());
